47ef1cabdb3242c11475064df6a2b4b9185e1dec
kp10-x <krishnaprasadv8@gmail.com>
2020-11-23 12:51:20 +0530

    distanc evector routing

---
+++ Lab 7/distvectalgo.py
@@ -0,0 +1,53 @@
+class Network:
+    def __init__(self,n):
+        self.matrix=[]
+        self.n=n 
+        
+    def addlink(self, u, v, w):  
+        self.matrix.append((u, v, w)) 
+        
+    def printtable(self, dist,src):
+        print("Vector Table of {}".format(chr(ord('A')+src)))
+        print("{0}\t{1}".format("Dest", "cost"))  
+        for i in range(self.n):  
+            print("{0}\t{1}".format(chr(ord('A')+i), dist[i]))  
+        
+    # Use Bellman Ford for weights   
+    def algo(self, src):  
+        
+        dist = [99] * self.n 
+        dist[src] = 0
+
+        for _ in range(self.n - 1):  
+            for u, v, w in self.matrix:  
+                if dist[u] != 99 and dist[u] + w < dist[v]:  
+                        dist[v] = dist[u] + w  
+        # print all distance  
+        self.printtable(dist,src)  
+
+
+def main():
+    matrix=[]
+    print("Enter No. of Nodes : ")
+    n=int(input())
+    print("Enter the Adjacency Matrix :")
+    for i in range(n):
+        m=list(map(int,input().split(" ")))
+        matrix.append(m)
+    g=Network(n)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j]==1:
+                g.addlink(i,j,1)
+    
+    for _ in range(n):
+        g.algo(_)
+main()
+
+
+# input as:
+# 0 1 1 99 99
+# 1 0 99 99 99
+# 1 99 0 1 1
+# 99 99 1 0 99
+# 99 99 1 99 0
